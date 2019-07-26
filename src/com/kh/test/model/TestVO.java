4e476ee72a5dc25197c8ef33753b19917732dcb0
user1 <user1@user1-PC>
2019-07-26 10:43:24 +0900

    TestVO 미완성상태

---
+++ src/com/kh/test/model/TestVO.java
@@ -0,0 +1,14 @@
+package com.kh.test.model;
+
+public class TestVO {
+	private String naem;
+
+	public String getNaem() {
+		return naem;
+	}
+
+	public void setNaem(String naem) {
+		this.naem = naem;
+	}
+	
+}
